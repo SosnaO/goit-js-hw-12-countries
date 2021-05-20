6599162031ed5ea34e7233dff6943d0c36f13464
SosnaO <73584691+SosnaO@users.noreply.github.com>
2021-05-20 21:25:50 +0300

    2

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,7 @@
 
 
 //fetch('https://restcountries.eu/#api-endpoints-name')
-fetch("https://restcountries.eu/rest/v2/name/Ukraine")
+fetch("https://restcountries.eu/rest/v2/name/Switzerland")
   .then(response => {
     return response.json();
 
