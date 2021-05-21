0fd6f37fa30f9effb0c1b18573bcf64fd717a0a4
SosnaO <73584691+SosnaO@users.noreply.github.com>
2021-05-21 20:54:59 +0300

    1

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,13 +1,20 @@
 
 
 //fetch('https://restcountries.eu/#api-endpoints-name')
+import './sass/main.scss';
+import CountriesTPL from './index.html';
+
+
+
 fetch("https://restcountries.eu/rest/v2/name/Switzerland")
   .then(response => {
     return response.json();
 
   })
   .then(name => {
-console.log(name)
+    console.log(name)
+    const markup = CountriesTPL(name);
+    console.log(markup);
   })
   .catch(error => {
     console.log(error);
