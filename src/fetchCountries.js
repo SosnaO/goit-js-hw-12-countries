7557f3da0d89ba17a7180d822c15a86fb258d669
SosnaO <73584691+SosnaO@users.noreply.github.com>
2021-05-22 18:51:12 +0300

    5

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -7,28 +7,50 @@ import CountriesTPL from './countries.hbs';
 
 const refs = {
 
-  cardContainer:document.querySelector('.body')
+  cardContainer: document.querySelector('.card-container'),
+  enterContainer: document.querySelector('#name-input'),
+
+}
+console.log(refs.enterContainer)
+
+refs.enterContainer.addEventListener('input', onEnterContainer)
+function onEnterContainer(event) {
+  event.preventDefault();
+  //const form = event.currentTarget;
+  const searchQuery = event.currentTarget.value;
+
+
+
+
+  console.log(searchQuery)
+
+
+fetchCountries(searchQuery)
+.then(renderCountriesCard)
+    .catch(error => {
+      console.log(error);
+    });
+
+  //refs.enterContainer.textContent = event.currentTarget.value;
+
+}
+//console.log(onEnterContainer)
+
+function fetchCountries(searchQuery) {
+
+  return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
+    .then(response => {
+      return response.json();
+    })
+    // .then(renderCountriesCard)
+    // .catch(error => {
+    //   console.log(error);
+    // });
 
 }
 
+function renderCountriesCard(name) {
+  const markup = CountriesTPL(name);
+    refs.cardContainer.innerHTML = markup;
 
-
-
-
-fetch("https://restcountries.eu/rest/v2/name/Switzerland")
-  .then(response => {
-    return response.json();
-
-  })
-  .then(name => {
-    console.log(name)
-    const markup = CountriesTPL(name);
-    console.log(markup);
-    body.innerHTML = markup;
-  })
-  .catch(error => {
-    console.log(error);
-
-  });
-
-
+}
